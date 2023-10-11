c1afab529e39525b6b7d3728d0a244c09c51a61c
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-11 16:30:06 +0000

    rails g board

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Board resource:
+
+  # CREATE
+  post("/insert_board", { :controller => "boards", :action => "create" })
+          
+  # READ
+  get("/boards", { :controller => "boards", :action => "index" })
+  
+  get("/boards/:path_id", { :controller => "boards", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_board/:path_id", { :controller => "boards", :action => "update" })
+  
+  # DELETE
+  get("/delete_board/:path_id", { :controller => "boards", :action => "destroy" })
+
+  #------------------------------
+
   # Routes for the Post resource:
 
   # CREATE
